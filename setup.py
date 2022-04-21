a294667774bc7b50592d5bf57535d7fe4c90fcdf
Thijs Janssen <thijs@seecr.nl>
2022-04-21 14:31:53 +0200

    TJ: version 0.1.3

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ here = pathlib.Path(__file__).parent.resolve()
 
 long_description = (here/"README.rst").read_text(encoding="utf-8")
 
-version='0.1.2'
+version='0.1.3'
 
 setup(
     name='autotest',
