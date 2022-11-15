aa1e163f2bc26da3ebcb9ec4b734bf5129834073
Thijs Janssen <thijs@seecr.nl>
2022-11-15 10:57:46 +0100

    JJ/TJ: license

--- autotest/fixtures.py
+++ autotest/fixtures.py
@@ -1,3 +1,25 @@
+## begin license ##
+#
+# "Autotest": a simpler test runner for python
+#
+# Copyright (C) 2022 Seecr (Seek You Too B.V.) https://seecr.nl
+#
+# This file is part of "Autotest"
+#
+# "Autotest" is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# "Autotest" is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with "Autotest".  If not, see <http://www.gnu.org/licenses/>.
+#
+## end license ##
 
 import tempfile         # for tmp_path
 import pathlib          # for tmp_path
