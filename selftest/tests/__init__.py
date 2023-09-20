589789e6b9ffca718ddba15f6e8ab94ba703511f
Thijs Janssen <thijs@seecr.nl>
2023-09-20 13:40:31 +0200

    TJ: rename in license

--- selftest/tests/__init__.py
+++ selftest/tests/__init__.py
@@ -1,22 +1,22 @@
 ## begin license ##
 #
-# "Autotest": a simpler test runner for python
+# "selftest": a simpler test runner for python
 #
-# Copyright (C) 2022 Seecr (Seek You Too B.V.) https://seecr.nl
+# Copyright (C) 2022-2023 Seecr (Seek You Too B.V.) https://seecr.nl
 #
-# This file is part of "Autotest"
+# This file is part of "selftest"
 #
-# "Autotest" is free software: you can redistribute it and/or modify
+# "selftest" is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# "Autotest" is distributed in the hope that it will be useful,
+# "selftest" is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with "Autotest".  If not, see <http://www.gnu.org/licenses/>.
+# along with "selftest".  If not, see <http://www.gnu.org/licenses/>.
 #
 ## end license ##
